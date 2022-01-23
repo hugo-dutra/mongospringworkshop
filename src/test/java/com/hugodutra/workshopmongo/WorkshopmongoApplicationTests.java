453b4e6fa6c6f81738be2d03061149d52e2d469c
hugo-dutra <hugo.dutra@hotmail.com>
2022-01-23 18:20:55 -0300

    Primeiro commit

---
+++ src/test/java/com/hugodutra/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugodutra.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
